7280b1388e1cec05649dc7a00676c1d2e67ea30a
ra7ma3atef <151402363+ra7ma3atef@users.noreply.github.com>
2025-08-04 22:54:24 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
 import re
 
 
