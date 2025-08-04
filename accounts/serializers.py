ce60c1f74f792636e2427727a94c534869481514
ra7ma3atef <151402363+ra7ma3atef@users.noreply.github.com>
2025-08-04 22:53:52 +0300

    fixed login logic

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -12,6 +12,7 @@ from rest_framework import serializers
 from .models import User, KidParentRelation
 
 
+
 class ChildSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
