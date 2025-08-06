2e4cad7bbceffe5f051784f8d3249d6dd6bb6a03
ghonim0007 <ghonem717@gmail.com>
2025-08-07 01:15:35 +0300

    feat: Add categories model and API endpoints

--- courses/admin.py
+++ courses/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from .models import Course
+from django.contrib.auth import get_user_model
+from .models import Course, Category
 
-admin.site.register(Course)
+User = get_user_model()
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'level', 'created_by', 'created_at')
+    list_filter = ('level', 'created_by')
+    search_fields = ('title', 'description')
+    filter_horizontal = ('categories',) # many2many
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "created_by":
+            kwargs["queryset"] = User.objects.filter(role='Admin')
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Category)
