517d47faf9895149868523a2768e3dc9eed2c370
Yara-Youssef79 <yara.nwesar79@gmail.com>
2025-09-06 07:44:18 +0300

    fix progress API data for courses

--- code4kids_backend/urls.py
+++ code4kids_backend/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('api/', include('lessons.urls')),
     path('api/', include('ratings.urls')),
     path('api/', include('games.urls')),
-    path('progress/', include('progress.urls')),
+    path('api/progress/', include('progress.urls')),
     path('upload/', upload_media, name='upload_media'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
